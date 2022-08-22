7bf0a56bd7e8f302ba7853b6354cb6fb1f08b3c1
grataoro <grataoro@gmail.com>
2022-08-22 08:37:59 -0300

    first commit

--- aws/main.tf
+++ aws/main.tf
@@ -1,4 +1,3 @@
-
 terraform {
   required_providers {
     aws = {
@@ -12,34 +11,65 @@ provider "aws" {         # Tipo deo provedor
   region = "us-east-1"   # região 
 }
 
+provider "aws" {         # Tipo deo provedor 
+  alias = "us-east-2"
+  region = "us-east-2"   # região 
+}
+
+
+# recurso que cria 3 maquinas EC2
 resource "aws_instance" "dev" {       # nome do recurso 
     count = 3                         # quantidade de maquinas
     ami = "ami-052efd3df9dad4825"     # imagem ami da maquina
     instance_type = "t2.micro"        # tipo de maquina 
-    key_name = "terraform"        # nome do par de chaves
+    key_name = "terraform"            # nome do par de chaves
     tags = {                          # tags
         Name = "dev${count.index}"    # count.index faz referência ao numero da maquina                                   
     }
-    vpc_security_group_ids = ["sg-06f4244ba70dc2430"] # define o security group para as maquinas 
+    vpc_security_group_ids = ["${aws_security_group.acesso_ssh.id}"] # define o security group para as maquinas 
 }
 
-# fonte https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
 
-resource "aws_security_group" "acesso_ssh" {   # define o recurso
-  name        = "acesso_ssh"                   # nome para nossa security_group
-  description = "acesso_ssh"                   # descrição da nossa security_group
-  
 
-  ingress {
-    description      = "tcp_ssh"      
-    from_port        = 22                    # porta de saída
-    to_port          = 22                    # posta de entrada
-    protocol         = "tcp"                 # tipo de conexão
-    cidr_blocks      = ["177.30.99.177/32"]  # ips que podem ter acesso
-    
-  }
+
+# recurso que cria uma maquinas EC2
+resource "aws_instance" "dev4" {       # nome do recurso 
+    ami = "ami-052efd3df9dad4825"     # imagem ami da maquina
+    instance_type = "t2.micro"        # tipo de maquina 
+    key_name = "terraform"        # nome do par de chaves
+    tags = {                          # tags
+        Name = "dev4"    # count.index faz referência ao numero da maquina                                   
+    }
+    vpc_security_group_ids = ["${aws_security_group.acesso_ssh.id}"] # define o security group para as maquinas 
+    depends_on = [aws_s3_bucket.dev4]
+}
+
+# recurso que cria uma maquinas EC2 na região us-east-2
+resource "aws_instance" "dev5" {       # nome do recurso 
+    provider = aws.us-east-2     # definimos o provedor passando seu alias
+    ami = "ami-092b43193629811af"     # imagem ami da maquina
+    instance_type = "t2.micro"        # tipo de maquina 
+    key_name = "terraform"        # nome do par de chaves
+    tags = {                          # tags
+        Name = "dev5"    # count.index faz referência ao numero da maquina                                   
+    }
+    vpc_security_group_ids = ["${aws_security_group.acesso_ssh_us-east-2.id}"] # define o security group para as maquinas 
+    depends_on = [aws_s3_bucket.dev4]
+}
+
+
+
+
+
+# recurso que cria um buckets no S3
+# fonte https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "dev4" {
+  bucket = "save-dev4"
+  acl    = "private"
 
   tags = {
-    Name = "allow_tls"   # Nome do nosso recurso 
+    Name        = "save-dev4"
+    # Environment = "Dev"
   }
 }
+
---
+++ aws/security_group.tf
@@ -0,0 +1,43 @@
+# fonte https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+
+# Recurso que cria um security group
+resource "aws_security_group" "acesso_ssh" {   # define o recurso
+  name        = "acesso_ssh"                   # nome para nossa security_group
+  description = "acesso_ssh"                   # descrição da nossa security_group
+  
+
+  ingress {
+    description      = "tcp_ssh"      
+    from_port        = 22                    # porta de saída
+    to_port          = 22                    # posta de entrada
+    protocol         = "tcp"                 # tipo de conexão
+    cidr_blocks      = ["177.30.99.177/32"]  # ips que podem ter acesso
+    
+  }
+
+  tags = {
+    Name = "allow_tls"   # Nome do nosso recurso 
+  }
+}
+
+
+
+resource "aws_security_group" "acesso_ssh_us-east-2" {   # define o recurso
+  provider   = aws.us-east-2                # sefinimos o provedor passando seu alias
+  name        = "acesso_ssh"                   # nome para nossa security_group
+  description = "acesso_ssh"                   # descrição da nossa security_group
+  
+
+  ingress {
+    description      = "tcp_ssh"      
+    from_port        = 22                    # porta de saída
+    to_port          = 22                    # posta de entrada
+    protocol         = "tcp"                 # tipo de conexão
+    cidr_blocks      = ["177.30.99.177/32"]  # ips que podem ter acesso
+    
+  }
+
+  tags = {
+    Name = "allow_tls"   # Nome do nosso recurso 
+  }
+}
